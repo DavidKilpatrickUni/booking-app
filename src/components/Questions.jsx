9db7b733362d92702b24bfbc1cfa338b535b624e
David Kilpatrick <Davidkilpatrick@outlook.com>
2024-04-02 01:30:22 +0100

    layout and keys

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -16,123 +17,195 @@ library.add(fas)
 
 const Questions = ({ children, questions }) => {
 
-
     const [show, setShow] = useState(false);
+    const params = useParams()
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const place = 'The Polwarth Apartment'
-
-
-
-
+    const place = params.place
 
     return (
-        <section className='text-center text-md-start'>
-
+        <section
+            className='text-center text-md-start'
+        >
 
             <Container>
                 <Row>
                     {children}
-                    <Col className='col-md-9 d-flex col-12 align-items-center'>
+                    <Col
+                        className='col-md-9 d-flex col-12 align-items-center'
+                    >
 
-                        <Accordion defaultActiveKey="0"
-                            className='w-100'>
+                        <Accordion
+                            defaultActiveKey="0"
+                            className='w-100'
+                        >
 
                             {questions.filter((question, index) => index < 5).map((question, index) => (
-                                <Accordion.Item eventKey={index}>
+                                <Accordion.Item
+                                    eventKey={index}
+                                    key={`Accordion ${index + 1}`}
+                                >
                                     <Accordion.Header>
-                                        <h6 className='fw-bold'>{question.question}</h6>
+                                        <h6
+                                            className='fw-bold'
+                                        >
+                                            {question.question}
+                                        </h6>
                                     </Accordion.Header>
                                     <Accordion.Body>
-                                        <p className='text-start'>{question.answer}</p>
+                                        <p
+                                            className='text-start'
+                                        >
+                                            {question.answer}
+                                        </p>
                                     </Accordion.Body>
                                 </Accordion.Item>
                             ))}
                         </Accordion>
-
                     </Col>
-                    <Col className='col-md-3 col-12 h-100 align-self-center text-center mt-2 mt-md-0'>
-                        <p className='d-none d-md-block fw-bold'>Still Looking?</p>
-                        <Button variant='outline-primary'>Ask A Question</Button>
+                    <Col
+                        className='col-md-3 col-12 h-100 align-self-center text-center mt-2 mt-md-0'
+                    >
+                        <p
+                            className='d-none d-md-block fw-bold'
+                        >
+                            Still Looking?
+                        </p>
+                        <Button
+                            variant='outline-primary'
+                        >
+                            Ask A Question
+                        </Button>
                     </Col>
                 </Row>
-                <Row className='mt-2'>
+                <Row
+                    className='mt-2'
+                >
                     <Col>
-                        <Button variant='outline-primary' onClick={handleShow}> View All Questions ({questions.length})</Button>
+                        <Button
+                            variant='outline-primary'
+                            onClick={handleShow}
+                        >
+                            View All Questions ({questions.length})
+                        </Button>
                     </Col>
                 </Row>
             </Container>
 
-
-            <Offcanvas show={show} onHide={handleClose} placement='end' className='w-75'>
-                <Offcanvas.Header closeButton>
+            <Offcanvas
+                show={show}
+                onHide={handleClose}
+                placement='end'
+                className='w-75'
+            >
+                <Offcanvas.Header
+                    closeButton
+                >
                     <Offcanvas.Title>
                         {place}
                     </Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-
-
-                    {questions.map((question) => (
+                    {questions.map((question, index) => (
                         <>
-                            <Container>
+                            <Container
+                                key={`Question ${index + 1}`}
+                            >
                                 <Row>
                                     <Col>
-                                        <p className='fw-bold'><FontAwesomeIcon icon='fa-comments' className='me-2 text-primary' />{question.question}</p>
+                                        <p
+                                            className='fw-bold'
+                                        >
+                                            <FontAwesomeIcon
+                                                icon='fa-comments'
+                                                className='me-2 text-primary'
+                                            />
+                                            {question.question}
+                                        </p>
                                     </Col>
                                 </Row>
                                 <Row>
-                                    <Col className=''>
-                                        <p className='mb-1'><q>{question.answer}</q></p>
+                                    <Col>
+                                        <p
+                                            className='mb-1'
+                                        >
+                                            <q>
+                                                {question.answer}
+                                            </q>
+                                        </p>
                                     </Col>
                                 </Row>
                                 <Row>
-                                    <Col className='col-sm-5 col-12 mb-2'>
-                                        <p className='text-secondary fst-italic m-0'>
+                                    <Col
+                                        className='col-sm-5 col-12 mb-2'
+                                    >
+                                        <p
+                                            className='text-secondary fst-italic m-0'
+                                        >
                                             Replied: {question.answerTime}
                                         </p>
                                     </Col>
-                                    <Col className='col-sm-7 col-12'>
-                                        <Row className='d-flex flex-column flex-sm-row justify-content-center text-center'>
-                                            <Col className='col-sm-6 col-12'>
-                                                <p className='text-sm-end m-0' >
-                                                    <span className='text-success'>
-                                                        <FontAwesomeIcon icon='fa-thumbs-up' className='me-2 text-success' />
+                                    <Col
+                                        className='col-sm-7 col-12'
+                                    >
+                                        <Row
+                                            className='d-flex flex-column flex-sm-row justify-content-center text-center'
+                                        >
+                                            <Col
+                                                className='col-sm-6 col-12'
+                                            >
+                                                <p
+                                                    className='text-sm-end m-0'
+                                                >
+                                                    <span
+                                                        className='text-success'
+                                                    >
+                                                        <FontAwesomeIcon
+                                                            icon='fa-thumbs-up'
+                                                            className='me-2 text-success'
+                                                        />
                                                         Helpful
                                                     </span>
-
                                                 </p>
                                             </Col>
-                                            <Col className='col-sm-6 col-12'>
-                                                <p className='m-0' >
-                                                    <span className='text-danger'>
-                                                        <FontAwesomeIcon icon='fa-thumbs-down' className='me-2 text-danger' />
+                                            <Col
+                                                className='col-sm-6 col-12'>
+                                                <p
+                                                    className='m-0'
+                                                >
+                                                    <span
+                                                        className='text-danger'
+                                                    >
+                                                        <FontAwesomeIcon
+                                                            icon='fa-thumbs-down'
+                                                            className='me-2 text-danger'
+                                                        />
                                                         Not Helpful
                                                     </span>
                                                 </p>
                                             </Col>
                                         </Row>
-
                                     </Col>
                                 </Row>
                             </Container>
-
-
-
                             <hr />
                         </>
                     ))}
 
-
-
                 </Offcanvas.Body>
-                <div className='sticky-bottom border w-100 p-3 bg-white'>
-                    <Button className='w-100' size='lg'>Ask Question</Button>
+                <div
+                    className='sticky-bottom border w-100 p-3 bg-white'
+                >
+                    <Button
+                        className='w-100'
+                        size='lg'
+                    >
+                        Ask Question
+                    </Button>
                 </div>
             </Offcanvas>
-
         </section>
     )
 }
